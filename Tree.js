16534daa9ab4fb3b884f23922e64b26588b2f939
Gustavo Miño <gus.desarrollo.web@gmail.com>
2025-11-10 08:05:05 -0300

    Proyecto de Arbol binario de busqueda completo

--- Tree.js
+++ Tree.js
@@ -49,36 +49,180 @@ export class Tree {
     return node;
   }
 
-  //Eliminar un valor en el arbol
-  deleteItem(value) {
-    //Hay que tener en cuenta si el elemento a eliminar tiene hijos o no los tiene
+  // Encontrar en numero minimo para reemplazar
+  findMin(node) {
+    while (node.left !== null) {
+      node = node.left;
+    }
+    return node;
+  }
+
+  // Eliminar un nodo
+  deleteItem(value, node = this.root) {
+    if (node === null) {
+      return null;
+    }
+
+    if (value < node.data) {
+      node.left = this.deleteItem(value, node.left);
+    } else if (value > node.data) {
+      node.right = this.deleteItem(value, node.right);
+    } else {
+      if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      }
+
+      const successor = this.findMin(node.right);
+      node.data = successor.data;
+      node.right = this.deleteItem(successor.data, node.right);
+    }
+
+    return node;
   }
 
   //Buscar y devolver el nodo con el valor dado
-  find(value) {}
+  find(value, node = this.root) {
+    if (node === null) {
+      return null;
+    }
+    if (node.data === value) {
+      return node;
+    }
+
+    const left = this.find(value, node.left);
+    const right = this.find(value, node.right);
+
+    return left || right;
+  }
 
   //Recorre el arbol por niveles. Se busca que funcione igual que forEach para un array. Alienta a que se resuelva por iteracion y recursion. Si no recibe una callback hay que presentar un error.
-  levelOrderForEach(callback) {}
+  levelOrderForEach(callback) {
+    if (!callback) {
+      throw new Error("Tienes que pasar una callback.");
+    }
+
+    if (this.root === null) return;
+
+    const cola = [this.root];
+
+    while (cola.length > 0) {
+      const node = cola.shift();
+
+      callback(node);
+
+      if (node.left !== null) {
+        cola.push(node.left);
+      }
+      if (node.right !== null) {
+        cola.push(node.right);
+      }
+    }
+  }
 
   //Recorrer el arbol segun sea inOrden, preOrden, postOrden
   //Debe funcionar igual que forEach anterior
-  inOrderForEach(callback) {}
-  preOrderForEach(callback) {}
-  postOrderForEach(callback) {}
+  inOrderForEach(callback, node = this.root) {
+    if (!callback) {
+      throw new Error("Tienes que pasar una callback.");
+    }
+
+    if (node === null) return;
+
+    this.inOrderForEach(callback, node.left);
+    callback(node);
+    this.inOrderForEach(callback, node.right);
+  }
+  preOrderForEach(callback, node = this.root) {
+    if (!callback) {
+      throw new Error("Tienes que pasar una callback.");
+    }
+
+    if (node === null) return;
+
+    callback(node);
+    this.preOrderForEach(callback, node.left);
+    this.preOrderForEach(callback, node.right);
+  }
+  postOrderForEach(callback, node = this.root) {
+    if (!callback) {
+      throw new Error("Tienes que pasar una callback.");
+    }
+
+    if (node === null) return;
+
+    this.postOrderForEach(callback, node.left);
+    this.postOrderForEach(callback, node.right);
+    callback(node);
+  }
 
   //Devuelve la altura (numero de aristas en el camino mas largo desde ese nodo hasta un nodo hoja) y si no se encuentra el nodo en el arbol devuelve null
-  height(value) {}
+  height(value) {
+    const node = this.find(value);
+    if (node === null) return null;
 
+    return this.calcularHeight(node);
+  }
+
+  calcularHeight(node) {
+    if (node === null) {
+      return -1;
+    }
+
+    const alturaIzquierda = this.calcularHeight(node.left);
+    const alturaDerecha = this.calcularHeight(node.right);
+
+    return Math.max(alturaIzquierda, alturaDerecha) + 1;
+  }
   //Devuelve la profundidad (numero de aristas en el camino desde ese nodo hasta el nodo raíz) y si no se encuentra el nodo en el arbol devuelve null
-  depth() {}
+  depth(value) {
+    return this.calcularDepth(value, this.root, 0);
+  }
+
+  calcularDepth(value, node, currDepth) {
+    if (node === null) return null;
+    if (node.data === value) return currDepth;
+
+    if (value < node.data) {
+      return this.calcularDepth(value, node.left, currDepth + 1);
+    } else {
+      return this.calcularDepth(value, node.right, currDepth + 1);
+    }
+  }
 
   //Verificar que el arbol esta balanceado, aplicar esta formula
   //para cada nodo del arbol, la diferencia de altura entre sus subarboles izq y der no es mayor que 1, y que cada subarboles tambien esten balanceados.
   //Es decir hay que comprobar cada nodo
-  isBalanced() {}
+  isBalanced(node = this.root) {
+    if (node === null) return true;
+
+    const heightLeft = this.calcularHeight(node.left);
+    const heightRight = this.calcularHeight(node.right);
+
+    const balanceo = Math.abs(heightLeft - heightRight);
+
+    if (
+      balanceo <= 1 &&
+      this.isBalanced(node.left) &&
+      this.isBalanced(node.right)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   //Escribe una funcion que reequilibre el arbol si no esta balanceado
-  rebalance() {}
+  rebalance() {
+    const valores = [];
+
+    this.inOrderForEach((node) => {
+      valores.push(node.data);
+    });
+
+    this.root = this.buildTree(valores);
+  }
 
   //Necesita el nodo raiz
   prettyPrint = (node = this.root, prefix = "", isLeft = true) => {
--- main.js
+++ main.js
@@ -1,8 +1,65 @@
 import { Tree } from "./Tree.js";
 
-//[1, 3, 5,2]
-const arbol = new Tree([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+function generarRandomArray(size, max) {
+  const array = [];
+  for (let i = 0; i < size; i++) {
+    array.push(Math.floor(Math.random() * max));
+  }
+  return array;
+}
 
-arbol.insert(4);
+const arbol = new Tree(generarRandomArray(25, 100));
+
+console.log(
+  `El arbol esta ${arbol.isBalanced() ? "Balanceado" : "Desbalanceado"}`
+);
+
+const levelOrderValues = [];
+arbol.levelOrderForEach((node) => levelOrderValues.push(node.data));
+console.log(`Por Niveles ---> ${levelOrderValues.join(", ")}`);
+
+const preOrderValues = [];
+arbol.preOrderForEach((node) => preOrderValues.push(node.data));
+console.log(`PreOrden    ---> ${preOrderValues.join(", ")}`);
+
+const inOrderValues = [];
+arbol.inOrderForEach((node) => inOrderValues.push(node.data));
+console.log(`InOrden     ---> ${inOrderValues.join(", ")}`);
+
+const postOrderValues = [];
+arbol.postOrderForEach((node) => postOrderValues.push(node.data));
+console.log(`PostOrden   ---> ${postOrderValues.join(", ")}`);
+
+arbol.prettyPrint();
+
+const numerosDesbalancear = [101, 150, 200, 250, 300, 350, 400];
+numerosDesbalancear.forEach((num) => arbol.insert(num));
+
+console.log(
+  `El arbol esta ${arbol.isBalanced() ? "Balanceado" : "Desbalanceado"}`
+);
+arbol.prettyPrint();
+
+arbol.rebalance();
+
+console.log(
+    `El arbol esta ${arbol.isBalanced() ? "Balanceado" : "Desbalanceado"}`
+);
+
+const levelOrderValuesReb = [];
+arbol.levelOrderForEach((node) => levelOrderValuesReb.push(node.data));
+console.log(`Por Niveles ---> ${levelOrderValuesReb.join(", ")}`);
+
+const preOrderValuesReb = [];
+arbol.preOrderForEach((node) => preOrderValuesReb.push(node.data));
+console.log(`PreOrden    ---> ${preOrderValuesReb.join(", ")}`);
+
+const inOrderValuesReb = [];
+arbol.inOrderForEach((node) => inOrderValuesReb.push(node.data));
+console.log(`InOrden     ---> ${inOrderValuesReb.join(", ")}`);
+
+const postOrderValuesReb = [];
+arbol.postOrderForEach((node) => postOrderValuesReb.push(node.data));
+console.log(`PostOrden   ---> ${postOrderValuesReb.join(", ")}`);
 
 arbol.prettyPrint();
